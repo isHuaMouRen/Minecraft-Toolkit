7ea6c8c91e8df5232c3c30f26fade30a6b0359d4
HuaMouRen <2825338723@qq.com>
2025-11-14 19:31:29 +0800

    feat(home): 添加 `Home` 页

--- MinecraftToolkit/MainWindow.xaml.cs
+++ MinecraftToolkit/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using MinecraftToolkit.Pages;
+using ModernWpf.Controls;
+using ModernWpf.Media.Animation;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static MinecraftToolkit.Class.Globals.Obj;
 
 namespace MinecraftToolkit
 {
@@ -16,9 +20,61 @@ namespace MinecraftToolkit
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Obj
+        private Dictionary<string, Type> PageMap = new Dictionary<string, Type>();//预加载Pages
+        private DrillInNavigationTransitionInfo FrameAnimation = new DrillInNavigationTransitionInfo();
+        #endregion
+
+        #region Func
+        public void Initialize()
+        {
+            try
+            {
+                logger.Info($"{this.Name} 开始初始化");
+
+                //预加载Page
+                void AddPage(Type t) {
+                    PageMap.Add(t.Name, t);
+                    logger.Info($"添加 {t.Name} 进入预加载PageMap");
+                };
+                AddPage(typeof(PageHome));
+
+
+                //选择默认page
+                navView.SelectedItem = navViewItem_Home;
+
+                logger.Info($"{this.Name} 完成初始化");
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
+            Initialize();
+        }
+
+        private void navView_SelectionChanged(ModernWpf.Controls.NavigationView sender, ModernWpf.Controls.NavigationViewSelectionChangedEventArgs args)
+        {
+            try
+            {
+                if (navView.SelectedItem is NavigationViewItem item)
+                {
+                    logger.Info($"用户选择: {item.Tag} 页");
+
+                    frame.Navigate(PageMap[$"Page{item.Tag}"], null, FrameAnimation);
+                }
+                else
+                    throw new Exception("非法的值");
+            }
+            catch (Exception ex)
+            {
+                logger.Error("fas fasasfdfasdfasdfasfas");
+            }
         }
     }
 }

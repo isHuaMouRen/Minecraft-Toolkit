198c311a7d88325200840f146d20f8496bd415da
HuaMouRen <2825338723@qq.com>
2025-11-14 18:04:18 +0800

    imp(globals): 添加一个logger对象

--- MinecraftToolkit/Class/Globals.cs
+++ MinecraftToolkit/Class/Globals.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using HuaZi.Library.Json;
+using HuaZi.Library.Logger;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -8,11 +11,21 @@ using System.Threading.Tasks;
 namespace MinecraftToolkit.Class
 {
     public static class Globals
-    {
+    {        
         public static class Var
         {
             public static readonly string ExecutePath = $"{Assembly.GetExecutingAssembly().Location}";//执行位置
-            public static readonly string Version = $"1.0.0-alpha.1";//版本号
+            public static readonly string Version = $"1.0.0-alpha.1";//版本号           
+        }
+
+        public static class Obj
+        {
+            public static Logger logger = new Logger(Path.Combine(Var.ExecutePath, "Logs"));//日志记录器
+        }        
+
+        public static class Func
+        {
+            
         }
     }
 }

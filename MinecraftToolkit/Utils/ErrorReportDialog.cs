8c4fc6ac1d16ef825732a2254aa68fb5fbc3e8b2
HuaMouRen <2825338723@qq.com>
2025-11-14 19:40:54 +0800

    feat(utils): 添加错误报告对话框工具

---
+++ MinecraftToolkit/Utils/ErrorReportDialog.cs
@@ -0,0 +1,51 @@
+﻿using MinecraftToolkit.Class;
+using ModernWpf.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using static MinecraftToolkit.Class.Globals.Obj;
+
+namespace MinecraftToolkit.Utils
+{
+    public static class ErrorReportDialog
+    {
+        public static async void Show(string title, string message, Exception ex)
+        {
+            logger.Error($"{title} {message}: {ex}");
+            var dialog = new ContentDialog
+            {
+                Title = title,
+                Content = new StackPanel
+                {
+                    Children =
+                    {
+                        new System.Windows.Controls.ProgressBar
+                        {
+                            Value=100,
+                            Margin=new System.Windows.Thickness(0,0,0,10),
+                            Foreground=new SolidColorBrush(Color.FromRgb(255,50,50))
+                        },
+                        new Label
+                        {
+                            Content=message,
+                            Margin=new System.Windows.Thickness(0,0,0,10)
+                        },
+                        new TextBox
+                        {
+                            Text=ex.ToString(),
+                            IsReadOnly=true
+                        }
+                    }
+                },
+                PrimaryButtonText = "继续",
+                SecondaryButtonText = "终止",
+                DefaultButton = ContentDialogButton.Primary
+            };
+            await DialogManager.ShowDialogAsync(dialog, null, (() => Environment.Exit(1)));
+        }
+    }
+}

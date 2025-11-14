608c7ec52b23b214caf3d2c15580312643dc3f7f
HuaMouRen <2825338723@qq.com>
2025-11-14 17:29:40 +0800

    update: 添加全局类

---
+++ MinecraftToolkit/Class/Globals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinecraftToolkit.Class
+{
+    public static class Globals
+    {
+        public static class Var
+        {
+            public static readonly string ExecutePath = $"{Assembly.GetExecutingAssembly().Location}";//执行位置
+            public static readonly string Version = $"1.0.0-alpha.1";//版本号
+        }
+    }
+}
